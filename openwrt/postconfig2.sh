f3339a15af2b3d9c461f7bed61b6cbe5ad1d4271
D0ot <1306793135@qq.com>
2019-06-07 00:17:38 +0800

    add pptp.uci and wlan.uci

--- openwrt/postconfig2.sh
+++ openwrt/postconfig2.sh
@@ -9,9 +9,12 @@ opkg install git
 opkg install openssh-client
 
 echo "pptp support"
-opkg install ppp-mod-pptp
-opkg install luci-proto-ppp
+opkg install ppp-mod-pptp kmod-nf-nathelper-extra luci-proto-ppp kmod-ipt-raw
 
 echo "static ip and host name"
 
 uci batch < ./add.uci
+
+echo "WLAN setttings"
+
+uci batch < .wlan.uci
