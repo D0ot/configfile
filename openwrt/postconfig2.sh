85b327f45c5d7fca2c53af27df7284b80cde611c
D0ot <1306793135@qq.com>
2019-06-07 12:10:21 +0800

    add mount.uci and samba.uci

--- openwrt/postconfig2.sh
+++ openwrt/postconfig2.sh
@@ -7,10 +7,20 @@ echo "Install common packages"
 opkg install vim
 opkg install git
 opkg install openssh-client
+opkg install usbutils
+opkg install kmod-fs-vfat
+opkg install kmod-fs-ext4
+opkg install shadow-useradd shadow-usermod shadow-userdel shadow-passwd shadow-newgrp
+
+echo "Mount point configure"
+uci batch < ./mount.uci 
+block umount && block mount
+
 
 echo "pptp support"
 opkg install ppp-mod-pptp kmod-nf-nathelper-extra luci-proto-ppp kmod-ipt-raw
 uci batch < ./pptp.uci
+cat "iptables -t raw -A OUTPUT -p tcp -m tcp --dport 1723 -j CT --helper pptp" >> /etc/firewall.user
 
 echo "static ip and host name"
 
@@ -19,3 +29,9 @@ uci batch < ./add.uci
 echo "WLAN setttings"
 
 uci batch < .wlan.uci
+
+echo "Config SAMBA"
+opkg list | grep -e "samba.*-server" | cut -f 1 -d ' ' | xarg opkg install
+opkg install luci-app-samba
+uci batch < ./samba.uci 
+
