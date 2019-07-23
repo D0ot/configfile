e28eb603095bd259b2b3d1a8d36797fc7ee3b2d2
D0ot <1306793135@qq.com>
2019-07-23 11:24:10 +0000

    fix bugs that chsrc.sh and swichssl will not really change the source file.

--- openwrt/scripts/chsrc.sh
+++ openwrt/scripts/chsrc.sh
@@ -1,3 +1,7 @@
 #!/bin/ash
 
-sed 's/\(https:\/\/\|http:\/\/\)\(.*\)\<releases\>\(.*\)/\1mirrors.tuna.tsinghua.edu.cn\/lede\/releases\3/g' /etc/opkg/distfeeds.conf
+SOURCE_FILE="/etc/opkg/distfeeds.conf"
+
+cp ${SOURCE_FILE} ${SOURCE_FILE}.old
+
+sed 's/\(https:\/\/\|http:\/\/\)\(.*\)\<releases\>\(.*\)/\1mirrors.tuna.tsinghua.edu.cn\/lede\/releases\3/g' ${SOURCE_FILE}.old > ${SOURCE_FILE}
--- openwrt/scripts/swichssl.sh
+++ openwrt/scripts/swichssl.sh
@@ -1,3 +1,6 @@
 #!/bin/ash
 
-sed 's/\(https\|http\)/https/g'
+SOURCE_FILE="/etc/opkg/distfeeds.conf"
+
+cp ${SOURCE_FILE} ${SOURCE_FILE}.http
+sed 's/\(https\|http\)/https/g' ${SOURCE_FILE}.http > ${SOURCE_FILE}
